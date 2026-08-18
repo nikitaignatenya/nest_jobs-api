73ca1c1bf240aa48938dd8616ae761f7f84e4533
Nikita Ignatenya <nik.ignatenya@gmail.com>
2026-08-18 12:11:45 +0300

    errors

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,31 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import HttpException from 'src/exceptions/HttpException';
 import iJobs from 'src/interfaces/interfaces';
-import { jobs } from 'src/storage';
 import { DatabaseService } from 'src/storage/database.service';
 
 @Injectable()
 export class JobsService {
-  jobs: iJobs[];
-  constructor(private db: DatabaseService) {
-    this.jobs = jobs;
-  }
+  constructor(private db: DatabaseService) {}
 
   async getData(): Promise<iJobs[]> {
     try {
       const sql = 'SELECT * FROM jobs ORDER BY id';
       return await this.db.query(sql);
-    } catch (error) {
-      throw new HttpException(400, { id: 1, message: `${error}` });
+    } catch (error: any) {
+      throw new HttpException(400, { id: 1, message: `${error.message}` });
     }
   }
 
   async getDataById(id: string): Promise<iJobs[]> {
     try {
       const sql = `SELECT * FROM jobs WHERE id = ${id}`;
-      return await this.db.query(sql);
-    } catch (error) {
-      throw new HttpException(400, { id: 1, message: `${error}` });
+      const data = await this.db.query(sql);
+      if (!data.length) throw new Error('There are not this ID');
+      return data;
+    } catch (error: any) {
+      throw new HttpException(400, { id: 1, message: `${error.message}` });
     }
   }
 
@@ -35,8 +33,8 @@ export class JobsService {
       const sql = `INSERT INTO jobs (title, company, description, salaryFrom, salaryTo, currency, location, remote, employmentType, experience, skills) VALUES
       ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, ARRAY[$11]) RETURNING *`;
       return await this.db.query(sql, values);
-    } catch (error) {
-      throw new HttpException(400, { id: 1, message: `${error}` });
+    } catch (error: any) {
+      throw new HttpException(400, { id: 1, message: `${error.message}` });
     }
   }
 
@@ -56,24 +54,24 @@ export class JobsService {
 
       const result = await this.db.query(sql, [...values, id]);
       return result;
-    } catch (error) {
-      throw new HttpException(400, { id: 1, message: `${error}` });
+    } catch (error: any) {
+      throw new HttpException(400, { id: 1, message: `${error.message}` });
     }
   }
   async deleteData(id: string): Promise<iJobs[]> {
     try {
-      const sqlDelete = 'DELETE FROM jobs WHERE id = $1;';
-      const sqlAllSelected = 'SELECT * FROM jobs';
-      await this.db.query(sqlDelete, [id]);
-
       if (!(await this.getDataById(id)).length)
         throw new Error('There are no this ID');
 
-      return await this.db.query(sqlAllSelected);
-    } catch (error) {
-      console.log(error);
+      const sqlDelete = 'DELETE FROM jobs WHERE id = $1;';
+      const sqlAllSelected = 'SELECT * FROM jobs';
+      await this.db.query(sqlDelete, [id]);
+      const data = await this.db.query(sqlAllSelected);
+      if (!data.length) throw new Error('List is clear');
 
-      throw new HttpException(400, { id: 1, message: `${error}` });
+      return data;
+    } catch (error: any) {
+      throw new HttpException(401, { id: 1, message: `${error.message}` });
     }
   }
 }
